8a962110e41b8f95b614a73ff75ba852a4636726
ZachLTech <zlopezzl2021@gmail.com>
2024-09-29 12:07:52 -0400

    fixed long schedules

--- src/frontend/app/components/week_view.vue
+++ src/frontend/app/components/week_view.vue
@@ -66,34 +66,6 @@
             <div class="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100" style="grid-template-rows: repeat(48, minmax(3.5rem, 1fr))">
               <div ref="containerOffset" class="row-end-1 h-7" />
               <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">12AM</div>
-              </div>
-              <div />
-              <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">1AM</div>
-              </div>
-              <div />
-              <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">2AM</div>
-              </div>
-              <div />
-              <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">3AM</div>
-              </div>
-              <div />
-              <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">4AM</div>
-              </div>
-              <div />
-              <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">5AM</div>
-              </div>
-              <div />
-              <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">6AM</div>
-              </div>
-              <div />
-              <div>
                 <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">7AM</div>
               </div>
               <div />
@@ -155,10 +127,6 @@
               <div />
               <div>
                 <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">10PM</div>
-              </div>
-              <div />
-              <div>
-                <div class="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">11PM</div>
               </div>
               <div />
             </div>
